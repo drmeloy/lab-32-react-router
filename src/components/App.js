7ef810a3e4c8f0e15f3e5c43f9361535f1440caf
drmeloy <drmeloy@gmail.com>
2020-02-05 13:21:36 -0700

    wrote home

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link
+} from 'react-router-dom';
+import Home from './home/Home.jsx';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      // Header
+      <Switch>
+        <Route exact path='/home' component={Home} />
+        // Route List of Characters
+      </Switch>
+    </Router>
+  )
 }
   
